61c42a0288740e4ff4f368f25522c1ba71f73119
leonardo <ljcjleonardo@gmail.com>
2018-05-16 21:57:29 -0300

    Crinado o primeiro projeto com angular-cli. Hello world

---
+++ primeiro-projeto-angular-cli/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com angular 2!</p>
+    `
+})
+
+export class MeuPrimeiroComponent{
+
+
+}
